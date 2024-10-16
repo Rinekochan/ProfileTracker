904e725f32605c29319315befc9ae704c4f7a6d8
Henry <viethoang9314@gmail.com>
2024-10-17 02:13:08 +1100

    Logic Development - Implement Local and Remote (Room vs Firestore) synchronisation mechanism

--- app/src/main/java/com/sdmd/assignment3/repository/ProfileRepository.kt
+++ app/src/main/java/com/sdmd/assignment3/repository/ProfileRepository.kt
@@ -7,6 +7,9 @@ import com.sdmd.assignment3.database.local.RoomTAG
 import com.sdmd.assignment3.database.remote.Firestore
 import com.sdmd.assignment3.database.remote.FirestoreTAG
 import com.sdmd.assignment3.model.Profile
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 const val ProfileRepositoryTAG = "ProfileRepository"
 // This repository helps syncing between local and remote database, and fetching data from local database
@@ -31,7 +34,13 @@ class ProfileRepository(
             profiles.forEach {
                 profileDao.insertProfile(it)
             }
+        } else {
+            // Launch a coroutine to synchronize databases without blocking, the app can display the updated list later
+            CoroutineScope(Dispatchers.Default).launch {
+                synchronizeDatabases()
+            }
         }
+
         Log.d(RoomTAG, "Local Read Success")
         return profiles
     }
@@ -55,5 +64,39 @@ class ProfileRepository(
         firestore.updateProfile(profile)  // Update remotely in Firestore
         profileDao.updateProfile(profile) // Update locally in Room
         Log.d(RoomTAG, "Local Update Success")
+    }
+
+    // Note: This sync process works because our local Room database is the source of truth
+    //       The remote database is to store it remotely so it won't be deleted if the app is uninstalled
+    @WorkerThread
+    private suspend fun synchronizeDatabases() {
+        Log.i(ProfileRepositoryTAG, "Synchronise local and remote databases")
+        if (isSyncPerformed) return  // Only perform sync once
+
+        val roomProfiles = profileDao.getAllProfiles()
+        val remoteProfiles = firestore.getAllProfiles()
+        Log.d(RoomTAG, "Current Local Database $roomProfiles")
+        Log.d(FirestoreTAG, "Current Remote Database $remoteProfiles")
+
+        val remoteProfilesIds = remoteProfiles.map { it.id }.toSet()
+
+        // Insert missing profiles from Room to Firestore
+        for (roomProfile in roomProfiles) {
+            if (!remoteProfilesIds.contains(roomProfile.id)) {
+                Log.d(ProfileRepositoryTAG, "Profile ${roomProfile.id} is missing remotely.")
+                firestore.insertProfile(roomProfile)
+            }
+        }
+
+        // Check for differences between Room and Firestore
+        for (roomProfile in roomProfiles) {
+            val firestoreProfile = remoteProfiles.find{ it.id == roomProfile.id }
+            if (roomProfile != firestoreProfile) {
+                Log.d(ProfileRepositoryTAG, "Profile ${roomProfile.id} is different remotely.")
+                firestore.updateProfile(roomProfile)
+            }
+        }
+
+        isSyncPerformed = true // Mark sync as done
     }
 }
